23f10cc56d872cbe1a8a3a2fc513363d51972e70
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-03 18:51:34 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,3 @@
-def greeting
-  puts "suck it big time"
-end
-
-greeting
-greeting
-
-num_of_attendees = 547
-
-puts "There are #{num_of_attendees} people coming to Baileys birthday party"
-
 def stylish_chef
   best_hairstyle = "Guy Fieri"
   return "Martha Stewart"
