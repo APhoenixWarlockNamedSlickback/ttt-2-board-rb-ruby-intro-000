babe03b91567bd71bb42bd8e91bb82e0eda94d58
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-03 18:51:15 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -8,3 +8,9 @@ greeting
 num_of_attendees = 547
 
 puts "There are #{num_of_attendees} people coming to Baileys birthday party"
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
